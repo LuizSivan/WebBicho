6447cd31918a76cf6b615823f29132a061c90443
Luiz Silva <juninhojsds012@gmail.com>
2024-02-01 03:33:00 -0300

    Começado construção do generic controller
    só parei pq a muié ta sendo chata

--- server/src/app/modules/generic/generic.controller.ts
+++ server/src/app/modules/generic/generic.controller.ts
@@ -1,5 +1,53 @@
-import {Controller} from '@nestjs/common';
+import {Get, Headers, HttpException, HttpStatus, Query} from '@nestjs/common';
+import {GenericEntity} from '../../shared/models/entities/generic-entity';
+import {Page} from '../../shared/models/classes/page';
+import {GenericService} from '../../shared/services/generic.service';
+import {FindOptionsOrder} from 'typeorm';
 
-@Controller('generic')
-export class GenericController {
+export class GenericController<T extends GenericEntity> {
+	
+	constructor(
+			private readonly service: GenericService<T>
+	) {
+	}
+	
+	@Get('list')
+	public async list(
+			@Headers('fields') fields: string[],
+			@Headers('relations') relations: string[],
+			@Headers('params') params: any[],
+			@Headers('order') order: FindOptionsOrder<T>,
+			@Query('page') page: number = 1,
+			@Query('size') size: number = 9,
+	): Promise<Page<T> | HttpException> {
+		try {
+			return this.service.list(
+					page,
+					size,
+					fields,
+					relations,
+					params,
+					order
+			);
+		} catch (e: any) {
+			throw new HttpException(
+					e.message ?? 'Internal Server Error',
+					HttpStatus.INTERNAL_SERVER_ERROR,
+			);
+		}
+	}
+	
+	@Get('get-by-id')
+	public async findById(
+			@Query('id') id: string,
+	): Promise<T | HttpException> {
+		try {
+			return this.service.findById(id);
+		} catch (e: any) {
+			throw new HttpException(
+					e.message ?? 'Internal Server Error',
+					HttpStatus.INTERNAL_SERVER_ERROR,
+			);
+		}
+	}
 }
