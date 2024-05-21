5f8f15bd4dd983ad54e272ec4f186b78aa001118
luiz.silva <cleverson.junior@limbersoftware.com.br>
2024-05-21 15:04:28 -0300

    Commit de teste no space

--- client/src/app/core/auth/auth.service.ts
+++ client/src/app/core/auth/auth.service.ts
@@ -7,58 +7,58 @@ import {HttpClient} from '@angular/common/http';
 import {User} from '../../shared/models/entities/user';
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: 'root'
 })
 export class AuthService {
-	
-	readonly endpoint: string = 'auth';
-	
-	constructor(
-			private cookieService: CookieService,
-			private http: HttpClient
-	) {
-	}
-	
-	setToken(token: string): void {
-		this.cookieService.set('jwtToken', token, 1, '/', '', false, 'Strict');
-	}
-	
-	getToken(): string {
-		return this.cookieService.get('jwtToken');
-	}
-	
-	removeToken(): void {
-		this.cookieService.delete('jwtToken');
-	}
-	
-	checkAuthStatus(): Observable<IAuthenticatedToken> {
-		return new HttpRequest<IAuthenticatedToken>(this.http)
-				.setEndpoint(this.endpoint)
-				.doGet();
-	}
-	
-	verifyAccount(token: string): Observable<User> {
-		return new HttpRequest<User>(this.http)
-				.setEndpoint(this.endpoint)
-				.setUri('verify-account')
-				.addParam('token', token)
-				.doPatch();
-	}
-	
-	
-	register(user: User): Observable<User> {
-		return new HttpRequest<User>(this.http)
-				.setEndpoint(this.endpoint)
-				.setUri('register')
-				.addBody(user)
-				.doPost();
-	}
-	
-	login(user: User): Observable<User> {
-		return new HttpRequest<User>(this.http)
-				.setEndpoint(this.endpoint)
-				.setUri('login')
-				.addBody(user)
-				.doPost();
-	}
+
+  readonly endpoint: string = 'auth';
+
+  constructor(
+    private cookieService: CookieService,
+    private http: HttpClient
+  ) {
+  }
+
+  setToken(token: string): void {
+    this.cookieService.set('jwtToken', token, 1, '/', '', false, 'Strict');
+  }
+
+  getToken(): string {
+    return this.cookieService.get('jwtToken');
+  }
+
+  removeToken(): void {
+    this.cookieService.delete('jwtToken');
+  }
+
+  checkAuthStatus(): Observable<IAuthenticatedToken> {
+    return new HttpRequest<IAuthenticatedToken>(this.http)
+      .setEndpoint(this.endpoint)
+      .doGet();
+  }
+
+  verifyAccount(token: string): Observable<User> {
+    return new HttpRequest<User>(this.http)
+      .setEndpoint(this.endpoint)
+      .setUri('verify-account')
+      .addParam('token', token)
+      .doPatch();
+  }
+
+
+  register(user: User): Observable<User> {
+    return new HttpRequest<User>(this.http)
+      .setEndpoint(this.endpoint)
+      .setUri('register')
+      .addBody(user)
+      .doPost();
+  }
+
+  login(user: User): Observable<User> {
+    return new HttpRequest<User>(this.http)
+      .setEndpoint(this.endpoint)
+      .setUri('login')
+      .addBody(user)
+      .doPost();
+  }
 }
