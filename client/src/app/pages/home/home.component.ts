c99619efccc8aa371671e1fde91d32c653ed4046
Luiz Silva <cleverson.silva@viasoft.com.br>
2025-01-17 14:33:06 -0300

    Migrando para o Angular 19

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -5,48 +5,49 @@ import {WhereParam} from '../../shared/types/where-param';
 import {Post} from '../../shared/models/entities/post';
 
 @Component({
-  selector: 'wb-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'wb-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss'],
+	standalone: false
 })
 export class HomeComponent {
-
-  constructor(
-    private themeService: ThemeService,
-    private postService: PostService,
-  ) {
-  }
-
-  get containerClass(): any {
-    return {
-      'layout-theme-light': this.themeService.config().colorScheme === 'light',
-      'layout-theme-dark': this.themeService.config().colorScheme === 'dark',
-      'layout-overlay': this.themeService.config().menuMode === 'overlay',
-      'layout-static': this.themeService.config().menuMode === 'static',
-      'layout-static-inactive': this.themeService.state.staticMenuDesktopInactive && this.themeService.config().menuMode === 'static',
-      'layout-overlay-active': this.themeService.state.overlayMenuActive,
-      'layout-mobile-active': this.themeService.state.staticMenuMobileActive,
-      'p-input-filled': this.themeService.config().inputStyle === 'filled',
-      'p-ripple-disabled': !this.themeService.config().ripple
-    };
-  }
-
-  changeTheme(): void {
-    this.themeService.switchTheme();
-    const sp: WhereParam<Post>[] = [
-      {
-        'between-id': ['uuid1', 'uuid2'],
-        user: {
-          id: 'uuid3',
-          name: '',
-          comment: {
-            id: 'uuid4',
-          }
-        }
-      }
-    ];
-    this.postService.getList(0, 10);
-  }
+	
+	constructor(
+			private themeService: ThemeService,
+			private postService: PostService,
+	) {
+	}
+	
+	get containerClass(): any {
+		return {
+			'layout-theme-light': this.themeService.config().colorScheme === 'light',
+			'layout-theme-dark': this.themeService.config().colorScheme === 'dark',
+			'layout-overlay': this.themeService.config().menuMode === 'overlay',
+			'layout-static': this.themeService.config().menuMode === 'static',
+			'layout-static-inactive': this.themeService.state.staticMenuDesktopInactive && this.themeService.config().menuMode === 'static',
+			'layout-overlay-active': this.themeService.state.overlayMenuActive,
+			'layout-mobile-active': this.themeService.state.staticMenuMobileActive,
+			'p-input-filled': this.themeService.config().inputStyle === 'filled',
+			'p-ripple-disabled': !this.themeService.config().ripple
+		};
+	}
+	
+	changeTheme(): void {
+		this.themeService.switchTheme();
+		const sp: WhereParam<Post>[] = [
+			{
+				'between-id': ['uuid1', 'uuid2'],
+				user: {
+					id: 'uuid3',
+					name: '',
+					comment: {
+						id: 'uuid4',
+					}
+				}
+			}
+		];
+		this.postService.getList(0, 10);
+	}
 }
 
 /*const where: any[] = [
